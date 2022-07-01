d240a4bc4b8c4daecc364a3563e540274102b113
PickleRick <bmkersey@gmail.com>
2022-07-01 02:49:25 -0400

    added CRUD routes for comments and updated some other routes to include comment model

--- models/index.js
+++ models/index.js
@@ -40,20 +40,21 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-Post.hasMany(Comment,{
-    foreignKey: 'post_id'
-});
 
 Comment.belongsTo(User,{
-    foreignKey: 'comment_id'
+    foreignKey: 'user_id'
 });
 
 Comment.belongsTo(Post,{
-    foreignKey:'comment_id'
+    foreignKey:'post_id'
 });
 
 User.hasMany(Comment,{
     foreignKey:'user_id'
-})
+});
+
+Post.hasMany(Comment,{
+    foreignKey: 'post_id'
+});
 
 module.exports = { User, Post, Vote, Comment };
