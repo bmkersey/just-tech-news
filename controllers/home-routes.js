5f933a27f0b94753b207abb3da0970c9a2f78a29
PickleRick <bmkersey@gmail.com>
2022-07-04 14:15:14 -0400

    added sessions, and ability to logout.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-router.get('/', (req, res) => {   
+router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
         attributes: [
             'id',
@@ -38,6 +39,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
     res.render('login');
 });
 
